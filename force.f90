0ee407b48f75d3090aab0a75656b2f8a0f8b7705
Mary Benage <mary.benage@eas.gatech.edu>
2012-04-06 18:53:35 +0000

    update force
    git-svn-id: https://subversion.assembla.com/svn/gt-cs6730-proj2-sp12/trunk@10 bdfef739-db73-45c7-a785-43d68237f13c

--- force.f90
+++ force.f90
@@ -1,13 +1,14 @@
-      Subroutine Force(f,Upot)
+      Subroutine Force(f,Upot) 
       Implicit None
  
 !     Calculate The Forces And Potential Energy
        
       Include 'globals.inc'
-
+      Double Precision:: f(Maxatom), Upot
       Integer:: I,J,Type1, Type2
-      Double Precision:: Dx,Dy,Ff,R_square,R_square_i,R_six_i,Rcut, Rcutsq
-      DOUBLE PRECISION::  K_bond, Rcut_bond,Rcutsq_bond 
+      Double Precision:: Dx, Dy, Ff, R_square, R_square_i, R_six_i, Rcut, Rcutsq,&
+			 sigma_square, K_bond, Rcut_bond,&
+			 Rcutsq_bond, eps, sigma, sqR_square    
 !**********Initialize the Forces, Potential Energy and Pressure to 0************
  
       DO I = 1,Natom
@@ -24,17 +25,17 @@
          DO J = I + 1,Natom
  
 ! Calculate the distance between the two atoms
-            Dx = Rxx(I) - Rxx(J)
-            Dy = Ryy(I) - Ryy(J)
+            Dx = Xx(I) - Xx(J)
+            Dy = Yy(I) - Yy(J)
  
 ! Apply the periodic boundary conditions
             Dx = Dx - Box*nint(Dx/Box)
             Dy = Dy - Box*nint(Dy/Box)
  
             R_square = Dx*Dx + Dy*Dy  
-! Determine the Lennard-Jones parameters dependent on the two atom types             
-             Type1 = atom_type(I)   !Array list of atom type
-             Type2 = atom_type(J)  
+! Determine the Lennard-Jones parameters dependent on the two atom types          
+             Type1 = AT(I)   !Array list of atom type
+             Type2 = AT(J)  
             
              ! Type 1 and 2 will be 1-4 integer of atom type
              Rcut = R_cut_matrix(Type1,Type2)
@@ -48,9 +49,10 @@
                R_square_i = 1.0/R_square
                sigma_square = sigma**2.0
                R_six_i = (R_square_i*sigma_square)**3.0
-               
+          
                Upot  = Upot + 4.00*eps*R_six_i*(R_six_i - 1.00) - Ecut
                Ff    = 48.0*eps*R_square_i*R_six_i*(R_six_i - 0.5)
+
                Press = Press + Ff
                Ff    = Ff*R_square_i
 !  Update the total force on atoms
@@ -65,14 +67,19 @@
 
 ! ROUGH DRAFT OF USING BONDS!
                 ! HEAD          TAIL1         TAIL2
-            IF (Type1==2 .AND. Type2==3 .OR. Type2=4) THEN
+            IF (Type1==2 .AND. Type2==3 .OR. Type2==4) THEN
              !A bond calculation required
+!QUESTION: should Energy_bond be Global?? Or, We can specify it here
+! DONG: The Bonds I still don't quite understand but Energy_bond, bond_Rcut 
+! should be global and defined from the read in of the data file. We 
+! probably don't need to reassign them to K-bond, etc as I did here. 
+! This is a rough draft. 
               K_bond = Energy_bond
               Rcut_bond = bond_Rcut
               Rcutsq_bond = Rcut_bond**2.0
               IF (R_square .Lt. Rcutsq_bond) THEN
                 sqR_square = sqrt(R_square)
-                Ff = -K_bond*(2.0-(Rcut_bond/sqR_square)
+                Ff = -K_bond*(2.0-(Rcut_bond/sqR_square))
                 Upot = Upot + K_bond*((sqR_square - Rcut_bond)**2.0)  !The bond potential at Rcut = 0 
                 Press = Press + Ff
                 Fx(I) = Fx(I) + Ff*Dx
@@ -92,3 +99,4 @@
  
       Return
       END SUBROUTINE
+
