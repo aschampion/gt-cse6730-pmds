b61c63a9f6daae29738669f446b55d4f9eabd4b6
Andrew Champion <andrew.champion@gmail.com>
2012-04-10 14:35:21 +0000

    Fixed force style run.
    git-svn-id: https://subversion.assembla.com/svn/gt-cs6730-proj2-sp12/trunk@36 bdfef739-db73-45c7-a785-43d68237f13c

--- pmds.f95
+++ pmds.f95
@@ -10,19 +10,26 @@ subroutine run_simulation(num_timesteps, pair_style)
 
   include 'globals.inc'
 
-  integer :: num_timesteps
+  integer :: num_timesteps, i
   character(*), intent(in) :: pair_style
+
+  #open(99, file='out.dump')
   
   Nstep = 0
 
   do while(Nstep.lt.num_timesteps)
-    if (mod(Nstep, 100).eq.0) write(*,'(A I4)') 'Running timestep: ', Nstep+1
+    if (mod(Nstep, 100).eq.0) write(*,'(A I8)') 'Running timestep: ', Nstep+1
+    if (mod(Nstep, 10000).eq.0) then
+      #write(99, '(I5)') Natom
+      #write(99, '(F13.3 F13.3)') (Xx(i), Yy(i), i=1,Natom)
+      #flush(99)
+    endif
     select case (pair_style)
       case ('soft')
         A_soft = 19.0*(Nstep/num_timesteps) + 1.0
         call force_soft()
         if (mod(Nstep, 100).eq.0) write(*,'(A I4)') 'Calculating soft force'
-      case ('lj/cut')
+      case ('lj')
         call force()
         if (mod(Nstep, 100).eq.0) write(*,'(A I4)') 'Calculating LJ force'
     end select
@@ -32,4 +39,6 @@ subroutine run_simulation(num_timesteps, pair_style)
 
     Nstep = Nstep + 1
   end do
+
+  #close(99)
 end subroutine
